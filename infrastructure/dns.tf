89febbc74e8ada1f04078d6c84b8ce299b626aef
Terrence Curran <tea@approachingpi.com>
2021-11-09 19:42:12 -0500

    .org added to terraform

--- infrastructure/dns.tf
+++ infrastructure/dns.tf
@@ -1,40 +1,40 @@
-# Route 53 for domain
-resource "aws_route53_zone" "main" {
+# .pizza
+resource "aws_route53_zone" "dot_pizza" {
   name = var.domain_name
   tags = var.common_tags
 }
 
-resource "aws_route53_record" "root-a" {
-  zone_id = aws_route53_zone.main.zone_id
+resource "aws_route53_record" "dot_pizza-a" {
+  zone_id = aws_route53_zone.dot_pizza.zone_id
   name    = var.domain_name
   type    = "A"
 
   alias {
-    name                   = aws_cloudfront_distribution.root_s3_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.root_s3_distribution.hosted_zone_id
+    name                   = aws_cloudfront_distribution.dot_pizza-www.domain_name
+    zone_id                = aws_cloudfront_distribution.dot_pizza-www.hosted_zone_id
     evaluate_target_health = false
   }
 }
 
-resource "aws_route53_record" "www-a" {
-  zone_id = aws_route53_zone.main.zone_id
+resource "aws_route53_record" "dot_pizza-www-a" {
+  zone_id = aws_route53_zone.dot_pizza.zone_id
   name    = "www.${var.domain_name}"
   type    = "A"
 
   alias {
-    name                   = aws_cloudfront_distribution.www_s3_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.www_s3_distribution.hosted_zone_id
+    name                   = aws_cloudfront_distribution.dot_pizza-www.domain_name
+    zone_id                = aws_cloudfront_distribution.dot_pizza-www.hosted_zone_id
     evaluate_target_health = false
   }
 }
 
-resource "aws_route53_record" "cert_validation" {
+resource "aws_route53_record" "dot_pizza-cert_validation" {
   for_each = {
-    for dvo in aws_acm_certificate.ssl_certificate.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.dot_pizza.domain_validation_options : dvo.domain_name => {
       name    = dvo.resource_record_name
       record  = dvo.resource_record_value
       type    = dvo.resource_record_type
-      zone_id = aws_route53_zone.main.zone_id
+      zone_id = aws_route53_zone.dot_pizza.zone_id
     }
   }
 
@@ -45,3 +45,59 @@ resource "aws_route53_record" "cert_validation" {
   type            = each.value.type
   zone_id         = each.value.zone_id
 }
+
+
+
+
+
+# .org
+# .org
+# .org
+
+resource "aws_route53_zone" "dot_org" {
+  name = var.domain_name_2
+  tags = var.common_tags
+}
+
+resource "aws_route53_record" "dot_org-a" {
+  zone_id = aws_route53_zone.dot_org.zone_id
+  name    = var.domain_name_2
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.dot_org-www.domain_name
+    zone_id                = aws_cloudfront_distribution.dot_org-www.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "dot_org-www-a" {
+  zone_id = aws_route53_zone.dot_org.zone_id
+  name    = "www.${var.domain_name_2}"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.dot_org-www.domain_name
+    zone_id                = aws_cloudfront_distribution.dot_org-www.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "dot_org-cert_validation" {
+  for_each = {
+  for dvo in aws_acm_certificate.dot_org.domain_validation_options : dvo.domain_name => {
+    name    = dvo.resource_record_name
+    record  = dvo.resource_record_value
+    type    = dvo.resource_record_type
+    zone_id = aws_route53_zone.dot_org.zone_id
+  }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = each.value.zone_id
+}
+
